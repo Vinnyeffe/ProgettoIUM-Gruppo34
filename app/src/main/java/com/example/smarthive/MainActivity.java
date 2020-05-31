4a86bafe6ef21ce5bab1eba55e6f71a56941b72d
maria-natale <m.natale19@studenti.unisa.it>
2020-05-31 19:05:02 +0200

    ..

--- app/src/main/java/com/example/smarthive/MainActivity.java
+++ app/src/main/java/com/example/smarthive/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.smarthive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
