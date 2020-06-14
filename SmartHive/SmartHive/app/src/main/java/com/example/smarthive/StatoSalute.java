6386ba6d4b518af56931443c87fa246ffe4d6b6d
maria-natale <m.natale19@studenti.unisa.it>
2020-06-14 22:03:00 +0200

    stato di salute e pesticidi

---
+++ SmartHive/SmartHive/app/src/main/java/com/example/smarthive/StatoSalute.java
@@ -0,0 +1,14 @@
+package com.example.smarthive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatoSalute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stato_salute);
+    }
+}
