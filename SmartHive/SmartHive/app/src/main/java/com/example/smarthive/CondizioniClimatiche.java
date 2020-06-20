4de1dadf624b4f77a746d80a41e59cbdeca6e188
Vinnyeffe <fabianovincenzo27@gmail.com>
2020-06-20 23:09:33 +0200

    sciamatura e condizioni climatiche

---
+++ SmartHive/SmartHive/app/src/main/java/com/example/smarthive/CondizioniClimatiche.java
@@ -0,0 +1,15 @@
+package com.example.smarthive;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CondizioniClimatiche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condizioni_climatiche);
+    }
+
+}
